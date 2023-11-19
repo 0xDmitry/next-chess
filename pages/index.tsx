3f0367f694beef7c0dd89f01638ecd8e75b0c556
0xDmitry <sign-69@mail.ru>
2023-11-19 21:18:54 +0300

    init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next Chess</title>
+        <meta name="description" content="Next Chess" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>TBD</div>
+    </>
+  );
+}
